770e7580766f0974001261d15f58f63e144653d0
von1336 <78800144+von1336@users.noreply.github.com>
2025-12-14 21:09:25 +0300

    Auto upload files

---
+++ project/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for project project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+]
+
